5a7bff288afc64a62749e9cfaac8c26204cfae22
Andrii Ishchuk <a_ishchuk22@ukr.net>
2024-11-13 11:59:47 +0300

    added necessary files and libraries

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// У файлі main.js напиши всю логіку роботи додатка.
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
